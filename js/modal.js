28c3fdfaf7c38f6044afdd0e8411914cfd8b69df
wittayasutt <wittayasutt@gmail.com>
2018-11-27 13:12:33 +0700

    iframe

--- js/modal.js
+++ js/modal.js
@@ -2,18 +2,20 @@ $(document).ready(function() {
 	var modal = document.getElementById('myModal')
 	var btn = document.getElementById('myBtn')
 	var span = document.getElementsByClassName('close')[0]
+	var iframe = $('.modal').find('iframe')
+
 	$('.open-video').click(function() {
 		modal.style.display = 'block'
-		iframe.attr('src', 'https://www.youtube.com/embed/Bhg45PNtsVs');
+		iframe.attr('src', 'https://www.youtube.com/embed/Bhg45PNtsVs')
 	})
 	$('#myModal .modal-content span').click(function() {
 		modal.style.display = 'none'
-		iframe.attr('src', '');
+		iframe.attr('src', '')
 	})
 	window.onclick = function(event) {
 		if (event.target == modal) {
 			modal.style.display = 'none'
-			iframe.attr('src', '');
+			iframe.attr('src', '')
 		}
 	}
 })
